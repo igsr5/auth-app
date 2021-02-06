469af8bf038885fb81321ea27033cc8df60575c4
ichigo-dev <ichigo@web-freework.com>
2021-02-07 00:08:43 +0900

    add Login.vue

---
+++ src/views/Login.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="login">
+    <input type="text" v-model="email"/>
+    <input type="password" v-model="password"/>
+    <button @click="login">login</button>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  data: function() {
+      return {
+          email: '',
+          password: ''
+        }
+    },
+    methods: {
+        login() {
+          window.location.href = '/'
+          }
+      }
+  }
+
+//export default class Login extends Vue {}
+</script>
