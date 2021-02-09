fe123ff20c88325f9ef8c72e067a62f9a87c3a16
ichigo-dev <ichigo@web-freework.com>
2021-02-09 20:58:23 +0900

    PostsでAPIから取得したデータを表示するように

--- src/views/Posts.vue
+++ src/views/Posts.vue
@@ -1,30 +1,40 @@
 <template>
   <ul>
-    <li v-for="(post, index) in posts" :key="index" @click="getIp">{{ post.title }}</li>
+    <li v-for="post in posts" :key="post.id">
+      {{ post.title }}
+    </li>
   </ul>
 </template>
 
 <script lang="ts">
+import Axios from 'axios'
 export default {
-    data: () => {
-        return {
-            posts: [
-              {title: 'aaaa'},
-              {title: 'bbbb'},
-              {title: 'cccc'},
-            ]
-          }
-      },
-      methods: {
-          getIp() {
-              this.axios.get('https://httpbin.org/get')
-                .then((response) => {
-                    alert(response.data.origin);
-                  })
-                .catch((e) => {
-                    alert(e)
-                  })
-            }
-        }
-  }
+  data: () => {
+    return {
+      posts: [],
+    };
+  },
+  mounted() {
+    Axios
+      .get("/api/v1/posts")
+      .then((response) => {
+        this.posts = response.data.data
+      })
+      .catch((e) => {
+        alert(e);
+      });
+  },
+  methods: {
+    getIp() {
+      this.axios
+        .get("/api/v1/posts")
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((e) => {
+          alert(e);
+        });
+    },
+  },
+};
 </script>
