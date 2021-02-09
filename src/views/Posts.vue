a625116f380065dc6cf9ba5b283da6da79a68239
ichigo-dev <ichigo@web-freework.com>
2021-02-09 20:05:56 +0900

    update Posts.vue

---
+++ src/views/Posts.vue
@@ -0,0 +1,19 @@
+<template>
+  <ul>
+    <li v-for="(post, index) in posts" :key="index">{{ post.title }}</li>
+  </ul>
+</template>
+
+<script lang="ts">
+export default {
+    data: () => {
+        return {
+            posts: [
+              {title: 'aaaa'},
+              {title: 'bbbb'},
+              {title: 'cccc'},
+            ]
+          }
+      }
+  }
+</script>
