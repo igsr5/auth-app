15e6858690d2a6cce18adc7199cac721e9205585
ichigo-dev <ichigo@web-freework.com>
2021-02-10 11:03:25 +0900

    add store action authRequest

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,24 +1,48 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-import createPersistedState from 'vuex-persistedstate'
+import Vue from "vue";
+import Vuex from "vuex";
+import createPersistedState from "vuex-persistedstate";
+import axios from "axios";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    token: ''
+    token: "",
   },
   mutations: {
     create(state, token) {
-      state.token = token
+      state.token = token;
     },
     destroy(state) {
-      state.token = ''
-    }
+      state.token = "";
+    },
   },
   actions: {
+    authRequest({ state, commit }, { email, password }) {
+      console.log(email);
+      console.log(password);
+      const headers = {
+        "Content-Type": "application/json",
+      };
+      axios
+        .post(
+          "auth",
+          {
+            email: email,
+            password: password,
+          },
+          {
+            headers: headers,
+          }
+        )
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    },
   },
-  modules: {
-  },
+  modules: {},
   plugins: [createPersistedState({ storage: window.sessionStorage })],
-})
+});
