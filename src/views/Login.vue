3d0c0de91c61639952e42be40d9a8eb8e0adf886
ichigo-dev <ichigo@web-freework.com>
2021-02-10 10:55:58 +0900

    udate Login.vue

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,29 +1,31 @@
 <template>
   <div class="login">
-    <input type="text" v-model="email"/>
-    <input type="password" v-model="password"/>
-    <button @click="login">login</button>
+    <input type="text" v-model="email" />
+    <input type="password" v-model="password" />
+    <button @click="setToken">login</button>
   </div>
 </template>
 
 <script lang="ts">
-
 export default {
   data: function() {
-      return {
-          email: '',
-          password: ''
-        }
+    return {
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    login() {
+      this.$store.commit("create", this.email);
+      this.$router.push({
+        name: "Home",
+      });
     },
-    methods: {
-        login() {
-          this.$store.commit('create', this.email)
-          this.$router.push({
-              name: 'Home'
-            })
-          }
-      }
-  }
+    setToken() {
+      this.$store.dispatch('authRequest', { email: this.email, password: this.password });
+    },
+  },
+};
 
 //export default class Login extends Vue {}
 </script>
